eb0d94af007b28e590b4b16026ec8b5648ae24cf
Zafer Kosktepe <zaferkosktepe@gmail.com>
2022-08-09 09:23:22 +0300

    1.surum

--- src/day01.java
+++
@@ -1,7 +0,0 @@
-public class day01 {
-
-    public static void main(String[] args) {
-
-        System.out.println("team15");
-    }
-}
---
+++ src/day01/c01.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class c01 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+
+}
